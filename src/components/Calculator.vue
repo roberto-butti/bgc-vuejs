53ef3e464c3f176c0983883d7f4e45711bccf986
Roberto Butti <roberto.butti@gmail.com>
2017-08-06 21:38:25 +0200

    first commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,43 @@
+<<template>
+  
+<div class="box is-info">
+    <p>
+
+Calculator
+<input v-model.number="rpm" type="number">
+<img src="../assets/img/bike-crankset.svg" width="64" alt="Crankset"/>
+<input v-model.number="crankset" type="number">
+<img src="../assets/img/bike-gears.svg" width="64" alt="Gears (Casette)"/>
+<input v-model.number="cassette" type="number">
+<img src="../assets/img/bike-spokes.svg" width="64" alt="Wheel Diameter"/>
+<input v-model.number="diameter_wheel" type="number">
+{{ rpm }}
+{{ crankset }}
+{{ cassette }}
+{{ diameter_wheel }}
+{{ crankset/cassette }}
+{{ (((crankset/cassette)*diameter_wheel*rpm)/1000000)*60 }} kilometer per hour
+
+
+    </p>
+</div>
+</template>
+
+<<script>
+export default {
+data () {
+    return {
+        rpm:70,
+        crankset:50,
+        cassette:11,
+        diameter_wheel:2098.58 // ( 622 + 23 + 23 ) * PI
+        }
+      
+  }
+}
+</script>
+
+<<style lang="sass">
+    
+    
+</style>
